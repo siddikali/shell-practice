0dc24e0105050492d10aa35b29ee6dbb19e0d0ac
siddikali <2004siddikali@gmail.com>
2026-06-03 11:10:51 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=modi
+PERSON2=musk
+
+echo "$PERSON1:: hey $PERSON2 hoe are you?"
+echo "$PERSON2:: hello $PERSON1, i am finne . how are you doing"
+echo "$PERSON2:: iam fine too , what's up?"
