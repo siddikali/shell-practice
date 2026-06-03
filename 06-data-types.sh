1b00dd09c6457ca3ea290720f021d4c26c8e8e13
siddikali <2004siddikali@gmail.com>
2026-06-03 11:43:29 +0530

    hello world started

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -2,7 +2,5 @@
  NUMBER1=100
  number2=50
 
- timestamp=$(date)
- echo "script executed at : $timestamp"
- sum=$(($NUMBER1+$number2))
- echo "sum of $number1 and number2 is : $sum"
+sum=$(($number1+$number2))
+echo "sum of two is : $sum"
