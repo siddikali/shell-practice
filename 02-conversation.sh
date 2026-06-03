7902a991547b378062b3c326b997d410aecfe326
siddikali <2004siddikali@gmail.com>
2026-06-03 13:09:08 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-echo "trump:: hey musk hoe are you?"
-echo "musk:: hello trump, i am finne . how are you doing"
-echo "musk:: iam fine too , what's up?"
+shell-practice/02-conversation.sh
---
+++ 09-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo"$person1: hi siddu how are you"
+echo"$person2: i am fine and you"
