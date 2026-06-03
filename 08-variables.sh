8c90a1c00a94f23e90ba4767755375e964ba6a28
siddikali <2004siddikali@gmail.com>
2026-06-03 12:54:57 +0530

    hello world started

---
+++ 08-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=$person1
+person2=$person2
+
+echo "$person1: hi $person1, hoe are you"
+echo "$person2: i am fine and how are you $person2"
+echo "$person1: fine ; what's up"
