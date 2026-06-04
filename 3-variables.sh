dbe3d6d44b7fcdd7530c48ddb59da48812e93597
siddikali <2004siddikali@gmail.com>
2026-06-04 11:17:04 +0530

    devops iam learning

--- 3-variables.sh
+++ 3-variables.sh
@@ -6,4 +6,4 @@ PERSON2=$2
 echo "$PERSON1: hello how are you $PERSON2"
 echo "$PERSON2: i am fine and what's up"
 echo "$PERSON1: nothing ; today are you going gym?"
-echo "$PERSON2: yes iam going, are you not coming
+echo "$PERSON2: yes iam going, are you not coming"
