367b03e7d2b2848063a8955da8382685eb9fe4c9
siddikali <2004siddikali@gmail.com>
2026-06-04 11:10:58 +0530

    devops iam learning

---
+++ 2-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=madhu
+PERSON2=siddu
+
+
+echo "PERSON1: hello how are you PERSON2"
+echo "PERSON2: i am fine and what's up"
+echo "PERSON1: nothing ; today are you going gym?"
+echo "PERSON2: yes iam going, are you not coming"
