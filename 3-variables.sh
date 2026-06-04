300d0f7390bb1e300f066c40cc81733c2359bbb8
siddikali <2004siddikali@gmail.com>
2026-06-04 11:16:03 +0530

    devops iam learning

---
+++ 3-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hello how are you $PERSON2"
+echo "$PERSON2: i am fine and what's up"
+echo "$PERSON1: nothing ; today are you going gym?"
+echo "$PERSON2: yes iam going, are you not coming
