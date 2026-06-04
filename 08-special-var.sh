d27a71743893554baf38d6d321ff257ec68b4355
siddikali <2004siddikali@gmail.com>
2026-06-04 13:30:26 +0530

    spacial variables started

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -3,9 +3,9 @@
 echo "all variables passed to the script: $@"
 echo "number of variables: $#"
 echo "script name: $0"
-echo "current directory: $pwd"
-echo "user running this script: $user"
-echo "home directory of user: $"
+echo "current directory: $PWD"
+echo "user running this script: $USER"
+echo "home directory of user: $home"
 echo "pid of the script: $$"
 sleep 10 &
 echo "pid of last command in background: $!"
