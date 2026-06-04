145b1c3a312c1f4bb125aca7ed20c0fb165c4a8d
siddikali <2004siddikali@gmail.com>
2026-06-04 13:09:38 +0530

    spacial varoables

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -7,5 +7,4 @@ echo "current directory: $pwd"
 echo "user running this script: $user"
 echo "home directory of user: $home"
 echo "pid of the script: $$"
-sleep 10 &
 echo "pid of last command in background: $!"
