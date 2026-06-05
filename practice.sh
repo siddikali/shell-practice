6692245a51467d9f564fe8f7cc2e00edb79f4163
siddikali <2004siddikali@gmail.com>
2026-06-05 14:08:55 +0530

    conditions started

--- practice.sh
+++ practice.sh
@@ -9,3 +9,20 @@ then
 else
     echo "you are running with root access"
 fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "mysqsl is not installed.... going to install it"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "installing mysql is... sucess"
+        exit 1
+    else
+        echo "installing mysql is... failure"
+    fi
+else
+    echo "my sql is already installed... nothing to do"
+fi    
